c868ef5efdea995ab2b94e457e9372765f8a6d05
Kesit Budi Kusumo <kusumo1920@icloud.com>
2022-04-06 20:35:35 +0700

    added new chapters and codes.

---
+++ ch10/write_message.py
@@ -0,0 +1,5 @@
+filename = "programming.txt"
+
+with open(filename, 'a') as file_object:
+    file_object.write("Myself, I like to use Python as primary programming language.\n")
+    file_object.write("The syntax, concepts & everything under the hood, it's just amazing!\n")
